3fc2a5f9835d039330c55a2b1d0132cceba4ff63
Érico Andrei <ericof@gmail.com>
2023-01-04 18:41:32 -0300

    Preparing release 0.1.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 0.1.0 (unreleased)
+## 0.1.0 (2023-01-04)
 
 - Fixtures `portal`, `http_request`, `installer`, `browser_layers`, `controlpanel_actions`
   [ericof]
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ description = "Pytest plugin to test Plone addons"
 
 setup(
     name="pytest-plone",
-    version="0.1.0.dev0",
+    version="0.1.0",
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
