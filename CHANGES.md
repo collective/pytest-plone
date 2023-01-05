60cae3efc329967964853febe9056a0112c2f595
Érico Andrei <ericof@gmail.com>
2023-01-05 16:43:27 -0300

    Back to development: 0.2.1

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.2.1 (unreleased)
+
+
+- Nothing changed yet.
+
+
 ## 0.2.0 (2023-01-05)
 
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ description = "Pytest plugin to test Plone addons"
 
 setup(
     name="pytest-plone",
-    version="0.2.0",
+    version="0.2.1.dev0",
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
